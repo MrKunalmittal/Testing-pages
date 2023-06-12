d1f08b105d7afbafed13aec10dfb68c4ef507c8b
MrKunalmittal <KUNAL>
2023-06-12 21:32:38 +0530

    Testing home

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router
-    basename='/home'>
+    basename='/Testing-pages'>
       <App/>
     </Router>
     
